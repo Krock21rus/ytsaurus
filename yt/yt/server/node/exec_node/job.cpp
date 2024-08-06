6b2aa60220a7efee19eef35891ea5a2bb58b4fce
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2024-08-06 15:30:57 +0300

    Detect job OOM
    - Detect when any process was killed by OOM killer in user job environment
      Always report here abort reason resource overdraft regardless of
      exit status of job main process - correct handing is not expected.
    
    - Detect when job proxy is killed by OOM killer
      Report that abort reason is resource overdraft.
    
    - Add option memory_oom_group for killing all tasks at OOM
    
    ---
    9e319d5b67dd6dc4b89671eace014e80a99bd1a8
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/681
    
    (cherry picked from commit d5f93f3b9b97f5fdff3ce0754b7879b894867fd1)
    (cherry picked from commit e789a26bbb41025c05dc3825af18f8bfcc6c3935)

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -2779,6 +2779,7 @@ TJobProxyInternalConfigPtr TJob::CreateConfig()
         proxyConfig->ForceIdleCpuPolicy = proxyDynamicConfig->ForceIdleCpuPolicy;
         proxyConfig->AbortOnUncaughtException = proxyDynamicConfig->AbortOnUncaughtException;
         proxyConfig->EnableStderrAndCoreLivePreview = proxyDynamicConfig->EnableStderrAndCoreLivePreview;
+        proxyConfig->CheckUserJobOOMKill = proxyDynamicConfig->CheckUserJobOOMKill;
         if (proxyDynamicConfig->JobEnvironment) {
             proxyConfig->JobEnvironment = PatchNode(proxyConfig->JobEnvironment, proxyDynamicConfig->JobEnvironment);
         }
@@ -3191,6 +3192,11 @@ std::optional<EAbortReason> TJob::DeduceAbortReason()
                     }
                     break;
                 }
+            }
+        }
+        if (auto processSignal = resultError.FindMatching(EProcessErrorCode::Signal)) {
+            if (processSignal->Attributes().Find<bool>("oom_killed").value_or(false)) {
+                return EAbortReason::ResourceOverdraft;
             }
         }
     }
